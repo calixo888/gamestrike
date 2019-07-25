597f839a225a910974e3e542c93683b024083d76
Calix Huang <47236374+calixo888@users.noreply.github.com>
2019-07-24 17:39:17 -0700

    Update views.py

--- gamestrike_project/gamestrike_app/views.py
+++ gamestrike_project/gamestrike_app/views.py
@@ -10,9 +10,26 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
-from crop_images import crop_images
 import datetime
+from PIL import Image
+import os
 
+def crop_images():
+    for file in os.listdir(os.path.abspath("media/profile_pictures")):
+        try:
+            filepath = os.path.abspath("media/profile_pictures") + "/" + file
+            img = Image.open(filepath)
+            width, height = img.size
+            crop_size = min(img.size)
+            left = (width - crop_size)/2
+            top = (height - crop_size)/2
+            right = (width + crop_size)/2
+            bottom = (height + crop_size)/2
+
+            img = img.crop((left, top, right, bottom))
+            img.save(filepath)
+        except Exception as e:
+            print(e)
 #################
 ## ERROR PAGES ##
 #################
